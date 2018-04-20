3d1768de59de1f8181f47819fc9b95877d96a40f
4698805@163.com <ace3846799>
2018-04-20 12:35:09 +1000

    change the heading of info window

--- WebApplication2/Scripts/map/addMarkers.js
+++ WebApplication2/Scripts/map/addMarkers.js
@@ -4,7 +4,8 @@
     var markers = listName.map(function (location, i) {
         var marker = new google.maps.Marker({
             position: location,
-            map: map
+            map: map,
+            
         });
         markers.push(marker);
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
@@ -12,7 +13,7 @@
                 var name = location.info +"\n";
                 var go_google = "view on google map";
                 var link_name = go_google.link("https://www.google.com/maps/search/?api=1&query=" + marker.position)
-                infowindowM.setContent(name+link_name);
+                infowindowM.setContent('<h2 id="firstHeading" class="firstHeading">'+location.info+'</h2>'+link_name);
                 infowindowM.open(map, marker);
             }
         })(marker, i));
