0ce04fa9c4a7a45d9da59801241e74af93d3274b
4698805@163.com <ace3846799>
2018-04-21 20:26:51 +1000

    insert ac values

---
+++ insert_ac.sql
@@ -0,0 +1,135 @@
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Homeless access point,launch housing',
+       '122 chapel st saint kilda',
+       'Short-term accommodation Transitional accommodation Advice Housing referrals',
+       -37.868812,
+       144.990482,
+       'Mon-Fri 9am-5m'
+);
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Ngwala willumbong',
+      '93 Wellington St, St Kilda VIC 3182',
+      'Aboriginal people, short time',
+      -37.856900,
+      144.988642,
+      'Mon-Fri 24/7');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Launch Housing',
+      '1/11 Chesterville Rd, Cheltenham VIC 3192',
+      'Short-term accommodation Transitional accommodation AdviceHousing referrals',
+      -37.961111,
+      145.056088,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Melbourne youth support service',
+      '9 King St, Melbourne VIC 3000',
+	  '',
+      -37.819968,
+      144.957434,
+      'Mon-Fri 9am-8pm Sat-sun 10am-6pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Launch housing collingwood',
+      '2a Oxford St, Collingwood VIC 3066',
+      '',
+      -37.804500,
+      144.984903,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Melbourne Homeless support line',
+      '',
+      '',
+      28.073545,
+      -80.602114,
+      'Mo-Sun');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Melbourne Youth Support Service (MYSS)',
+      '19 King Street, Melbourne VIC 3000',
+      '18 years to 24 years homeless people',
+      -37.819935,
+      144.957079,
+      'Mon-fri 9am-8am Sat,sun,public 10am-6pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Yarra Community Housing,unison seddon',
+      '112-122 Victoria St, Seddon VIC 3011',
+      'Long time housing',
+      -37.803550,
+      144.892136,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Unison seddon',
+      'Level 1, 1-13 Watton Street Werribee Vic. 3030',
+      'Long time housing',
+      -37.900247,
+      144.663762,
+      '9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Unison seddon',
+      '83 Ryrie Street, Geelong Vic. 3220',
+      'Long time housing',
+      -38.148321,
+      144.357050,
+      'Mon-fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Unison seddon',
+      '95 Wellington Street, Collingwood Vic. 3066',
+      'Long time housing',
+      -37.805769,
+      144.986000,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude)
+values('Unison seddon',
+      '42 Derby Street, Kensington Vic. 3031',
+      'Long time housing',
+      -37.796343,
+      144.927359);
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Unison seddon',
+      '40 Nicholson Street, Fitzroy Victoria 3065',
+      'Long time housing',
+      -37.806257,
+      144.973758,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Wesley Footscray outreach,support centre',
+      '310 Barkley st Footscray vic 3011',
+      'Single person,couple and families, Short time accomodation',
+      -37.798705,
+      144.893624,
+      'Wed 9.30am-12.15pm 1.30pm-4.00pm Thu 9.30am-12.15pm 1.30pm-4.00pm Fri 9.30am-12.15pm 1.30pm-4.00pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Merri Outreach Support Service',
+      '22 Lakeside Drive, Broadmeadows 3047',
+      'Help any homeless experiencing people',
+      -37.687523,
+      144.948156,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Merri Outreach Support Service',
+      '279 High Street, Northcote 3070',
+      'Help any homeless experiencing people',
+      -37.772157,
+      144.998245,
+      'Mon-Fri 9am-5pm');
+
+insert into dbo.ac_org(ac_name,ac_address,ac_condition,latitude,longitude,available_time)
+values('Merri Outreach Support Service',
+      '1 Holmes Street, Northcote 3070',
+      'Help any homeless experiencing people',
+      -37.783799,
+      145.005476,
+      'Mon-Fri 9am-5pm');
